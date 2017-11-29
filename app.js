59bfa62273dd80910ecbcf6cdd580a02c3d54fe6
smile <smile@192.168.1.7>
2017-11-29 16:29:03 +0400

    portfolio mvc

---
+++ app.js
@@ -0,0 +1,30 @@
+const ejs = require('ejs');
+const express = require('express');
+const app = express()
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const routes = require('./app/routes/routes')
+const dotenv = require('dotenv').load();
+
+// use  your engine template and configure the folder
+
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/')); // expression static for bootstrap ( in node_modules)
+app.use(express.static(__dirname + '/public'));  // find all ressource front
+
+
+
+// call routes
+routes(app)
+
+
+
+app.listen(8080)
+
+
+
+module.exports = app;
---
+++ app/routes/routes.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+module.exports =  (app) =>{
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.post('/email', (req, res) => {
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        host: 'smtp.gmail.com',
+        secure: true,
+        port: 465,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    });
+    let mail = {
+        from: req.body.email,
+        to: process.env.EMAIL,
+        html: req.body.name + " " + req.body.email + " "+ req.body.message
+    }
+   
+    transporter.sendMail(mail,(error, response) =>{
+        if (error) {
+            console.log("Mail non envoyé");
+            res.redirect('/')
+        } else {
+            console.log("Mail envoyé avec succès!")
+            res.redirect('/')
+        }
+        transporter.close();
+    });
+})
+
+}
