70aa338ddd8b3eac6b519c52d5c919e34840bffd
LauGR <laurent.gregoire974@gmail.com>
2018-04-24 13:43:18 +0400

    modif sans email check

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,37 +1,41 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv').load();
-module.exports =  (app) =>{
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+module.exports = (app) => {
+    app.get('/', (req, res) => {
+        res.render('index')
+    })
+app.get('/validationemail'),(req,res) => {
+    res.render('validationEmail')
+}
 
-app.post('/email', (req, res) => {
-    let transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        host: 'smtp.gmail.com',
-        secure: true,
-        port: 465,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS
+
+    app.post('/email', (req, res) => {
+        let transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            host: 'smtp.gmail.com',
+            secure: true,
+            port: 465,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASS
+            }
+        });
+        let mail = {
+            from: req.body.email,
+            to: process.env.EMAIL,
+            html: " nom : " + req.body.name + " email : " + req.body.email + " message : " + req.body.message
         }
-    });
-    let mail = {
-        from: req.body.email,
-        to: process.env.EMAIL,
-        html: req.body.name + " " + req.body.email + " "+ req.body.message
-    }
-   
-    transporter.sendMail(mail,(error, response) =>{
-        if (error) {
-            console.log("Mail non envoyé");
-            res.redirect('/')
-        } else {
-            console.log("Mail envoyé avec succès!")
-            res.redirect('/')
-        }
-        transporter.close();
-    });
-})
+
+        transporter.sendMail(mail, (error, response) => {
+            if (error) {
+                console.log("Mail non envoyé"); 
+                res.redirect('/nonvalidationemail')
+            } else {
+                console.log("Mail envoyé avec succès!")
+                res.redirect('/validationemail')
+            }
+            transporter.close();
+        });
+    })
 
 }
