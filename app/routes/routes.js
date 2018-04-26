158c671bd10819173698b92038d9b933cebfd93c
LauGR <laurent.gregoire974@gmail.com>
2018-04-26 08:22:14 +0400

    all ok

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,10 +7,7 @@ module.exports = (app) => {
     app.get('/validationemail', (req, res) => {
         res.render('validationEmail')
     })
-    app.get('/nonvalidationemail', (req, res) => {
-        res.render('nonvalidationEmail')
-    })
-
+   
 
     app.post('/email', (req, res) => {
         let transporter = nodemailer.createTransport({
@@ -32,7 +29,7 @@ module.exports = (app) => {
         transporter.sendMail(mail, (error, response) => {
             if (error) {
                 console.log("Mail non envoyé");
-                res.redirect('/nonvalidationemail')
+                res.redirect('/')
             } else {
                 console.log("Mail envoyé ")
                 res.redirect('/validationemail')
